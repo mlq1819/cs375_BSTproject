d4be32671719cb424f03823c002cf83bf5e24b35
ccui5 <31294007+ccui5@users.noreply.github.com>
2018-11-30 14:45:30 -0500

    new version of the test_generator.cpp and makefile
    makefile will generator one more .exe file, called test_generator

--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 P=BSTprogram
 R=Filereader
 Q=BSTree
+G=test_generator
 CFLAGS= -g -Wall
 
 all: $(P)
 
-$(P): $(P).o $(R).o $(Q).o
+$(P): $(P).o $(R).o $(Q).o $(G)
 	g++ $(CFLAGS) $(P).o $(R).o $(Q).o -o $(P)
 
 $(P).o: $(P).cpp $(R).h $(Q).h
@@ -16,9 +17,12 @@ $(R).o: $(R).cpp $(R).h
 
 $(Q).o: $(Q).cpp $(Q).h
 	g++ $(CFLAGS) -c $(Q).cpp -o $(Q).o
-	
+$(G):
+	g++ $(CFLAGS) $(G).cpp -o $(G)
+
 clean:
 	rm -rf $(P)
 	rm -rf $(P).o
 	rm -rf $(R).o
 	rm -rf $(Q).o
+	rm -rf $(G)
--- test_generator.cpp
+++ test_generator.cpp
@@ -7,12 +7,10 @@
 //============================================================================
 
 #include <iostream>
-#include "BinaryTree.h"
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <Array>
 #include <math.h>
 using namespace std;
 int left1(int i){
@@ -35,12 +33,6 @@ void testBalance(int num){
 		if(i%2==0)arr[i]=arr[parent(i)]+(arr[parent(parent(i))]-arr[parent(i)])/2;
 	}
 	for(int i=0;i<num-1;i++){
-//		cout<<arr[i]<<endl;
-	}
-}
-void testHighest(ifstream * f,int num){
-	for(int i = 0; i<num-1;i++){
-//		*f<<i<<" ";
 	}
 }
 
@@ -48,23 +40,27 @@ void testHighest(ifstream * f,int num){
 int main() {
 //	testBalance(64);
 	ofstream output;
+	cout<<"this file will generator <best.txt> and <worst.txt> files"<<endl;
+	cout<<"please input the range you want to test"<<endl;
+	int p;
+	cin>>p;
 	output.open("worst.txt");
-	for(int i=1;i<=pow(2,10);i++){
+	for(int i=1;i<=pow(2,p)-1;i++){
 		output<<i<<endl;
 	}
 	output.close();
 	output.open("best.txt");
-	int num=pow(2,10);
-	int arr[num];
+	int num=pow(2,p);
+	int arr[num-1];
 	arr[0]=num/2;
 	arr[1]=arr[0]/2;
 	arr[2]=arr[0]+arr[1];
-	for(int i=3;i<num;i++){
+	for(int i=3;i<num-1;i++){
 		if(i%2==1)arr[i]=arr[parent(i)]-(arr[parent(parent(i))]-arr[parent(i)])/2;
 		if(i%2==0)arr[i]=arr[parent(i)]+(arr[parent(parent(i))]-arr[parent(i)])/2;
 	}
-	arr[num-1]=num;
-	for(int i=0;i<num;i++){
+
+	for(int i=0;i<num-1;i++){
 		output<<arr[i]<<endl;
 		cout<<arr[i]<<endl;
 	}
